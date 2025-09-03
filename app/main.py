d8c6b62028446d0892ac42ff91aeb8c37f4eed1b
Dawid <kucharzjaw@gmail.com>
2025-09-03 11:44:20 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,21 +1,25 @@
+from __future__ import annotations
+from typing import Any
+
+
 class Animal:
 
-    alive = []
+    alive: list[Animal] = []
 
     def __init__(
             self,
             name: str,
-            health: int = 100,
-            hidden: bool = False
+            *args: Any,
+            **kwargs,
     ) -> None:
 
         self.name = name
-        self.health = health
-        self.hidden = hidden
+        self.health = 100
+        self.hidden = False
         Animal.alive.append(self)
 
     def die(self) -> None:
-        if self.health <= 0:
+        if self in Animal.alive:
             Animal.alive.remove(self)
 
     def __repr__(self) -> str:
@@ -35,8 +39,9 @@ class Herbivore(Animal):
 
 class Carnivore(Animal):
 
-    def bite(self, other: Herbivore) -> None:
+    def bite(self, other: Animal) -> None:
         if isinstance(other, Herbivore):
             if not other.hidden:
                 other.health -= 50
+                other.health = max(0, other.health)
                 other.die()
